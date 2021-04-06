70efd7d7309d87d8710d81bc85b608bc7585781c
Sam Cox <sjamcox@gmail.com>
2021-04-06 10:18:47 -0700

    Add routes for reviews service

---
+++ server/hosts.js
@@ -0,0 +1,3 @@
+module.exports = {
+  reviews: 'http://localhost:4006'
+};
--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const port = 3000;
+const axios = require('axios');
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000;
 const app = express();
+const hosts = require('./hosts');
 
-// For dev purposes
-// const morgan = require('morgan');
-// app.use(morgan('dev'));
-
+const morgan = require('morgan');
+app.use(morgan('dev'));
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
+
+app.get('/reviews/bundle', async (req, res) => {
+  try {
+    const { data: bundle } = await axios.get(`${hosts.reviews}/reviews_bundle.js`)
+    res.status(200).send(bundle)
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
+});
+
+app.post('/review', async (req, res) => {
+  try {
+    console.log(req.body)
+    const { data } = await axios.post(`${hosts.reviews}/review`, req.body)
+    res.status(201).send(data)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+app.get('/Reviews*', async (req, res) => {
+  try {
+    const { data } = await axios.get(`${hosts.reviews}${req.url}`)
+    res.status(200).send(data)
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
